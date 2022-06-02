900af1f6babe8a5a1c673b6cde403a39611410f1
anand <raghubanshi15anand@gmail.com>
2022-06-02 15:42:37 -0700

    Updates

---
+++ precache-manifest.9b39fe88d353f4f4d03177154b75286f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac548ffa9e896d9d072aaf08a739faf",
+    "url": "/newetaaide/index.html"
+  },
+  {
+    "revision": "87f40ea25a22d4b82aaf",
+    "url": "/newetaaide/static/css/2.f7375132.chunk.css"
+  },
+  {
+    "revision": "5548f20f352ea3275437",
+    "url": "/newetaaide/static/css/main.9b76c41c.chunk.css"
+  },
+  {
+    "revision": "87f40ea25a22d4b82aaf",
+    "url": "/newetaaide/static/js/2.5c9724e6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/newetaaide/static/js/2.5c9724e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5548f20f352ea3275437",
+    "url": "/newetaaide/static/js/main.bb6363e0.chunk.js"
+  },
+  {
+    "revision": "e82a4ba624b901d61483",
+    "url": "/newetaaide/static/js/runtime-main.cb061e52.js"
+  }
+]);
